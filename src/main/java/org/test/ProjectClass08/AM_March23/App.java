48190ddca249383e51e1bf46ea1477d42393415a
Dinesh1208 <dineshbecse92@gmail.com>
2023-03-23 21:41:46 +0530

    project structure

---
+++ src/main/java/org/test/ProjectClass08/AM_March23/App.java
@@ -0,0 +1,13 @@
+package org.test.ProjectClass08.AM_March23;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
